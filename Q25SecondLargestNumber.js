dab79222b92d16ce5559a60242473e0d493b1b1e
Tushar Babar <babartushar560@gmail.com>
2025-05-07 16:55:39 +0530

    Update Q25SecondLargestNumber.js

--- Q25SecondLargestNumber.js
+++ Q25SecondLargestNumber.js
@@ -1,3 +1,9 @@
+/*25. Find the Second Largest Number
+Description: Write a program to find the second largest number from a set of two numbers.
+Input: a = 10, b = 20 
+Output: 10
+*/
+
 let a=12;
 let b=45;
 if(a>b)
