ad2f7a8807c78d1c44d665b13291d435748843e6
Tushar Babar <babartushar560@gmail.com>
2025-05-07 16:57:00 +0530

    Update Q26MultiTwoNumber.js

--- Q26MultiTwoNumber.js
+++ Q26MultiTwoNumber.js
@@ -1,3 +1,9 @@
+/*26. Multiplication of Two Numbers
+Description: Write a program to multiply two numbers using loops.
+Input: a = 3, b = 5  */
+
+
+Output: 15
 let a = 3;
 let b = 5;
 let result = 0;
